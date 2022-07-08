2bf2c29a4a66fd2dce448c25d13af7617d95e144
AnhDaoHoang <anhdh1@smartosc.com>
2022-07-08 10:44:57 +0700

    fix next 4

--- routers/iphoneRouter.js
+++ routers/iphoneRouter.js
@@ -2,7 +2,9 @@ const ipController = require('../controllers/iphoneController');
 const router = require('express').Router();
 
 //get request
-router.get('/', ipController.getIp);
+router.get('/', function (req, res) {
+    res.send('Hello 333');
+});
 
 //post(add)
 router.post('/', ipController.addIp);
